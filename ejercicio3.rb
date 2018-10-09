b982c7102c243d42a692136996926ce7f0f3de47
ErickLedesma <ledesma.erick@gmail.com>
2018-10-08 23:20:24 -0300

    Second

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -16,6 +16,99 @@
 # 10. Opción 9: Muestra dos listas de personas, una por cada género.
 # 11. El menú debe repetirse hasta que el usuario ingrese la opción 10 (salir)
 
+alumnos = []
+alumnos = [ { Nombre: 'Juan', Edad: 28, Comuna: 'Providencia', Genero: 'Masculino'} ]
 
+print alumnos
+puts ''
+
+# 2
+puts "Ingresa un alumno"
+alumno = gets.chomp
+puts "Ingresa Edad"
+edad = gets.chomp.to_i
+puts "Ingresa Comuna"
+comuna = gets.chomp
+puts "Ingresa Genero"
+genero = gets.chomp
+h = { Nombre: alumno, Edad: edad, Comuna: comuna, Genero: genero }
+alumnos.push( h )
+
+print alumnos
+puts ''
+
+# 3
+puts "Ingresa un alumno"
+alumno = gets.chomp
+edad = alumno
+puts "Ingresa Edad"
+edad = gets.chomp.to_i
+puts "Ingresa Comuna"
+comuna = gets.chomp
+puts "Ingresa Genero"
+genero = gets.chomp
+h = { Nombre: alumno, Edad: edad, Comuna: comuna, Genero: genero }
+alumnos.push( h )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# inventario = {"Notebooks": 4, "PC_Escritorio": 6, "Routers": 10, "Impresoras": 6}
+# opcion = 0
+# while opcion != 7 do
+#     puts "Ingrese una opción
+#           1 - Agregar un item
+#           2 - Eliminar un item
+#           3 - Actualizar Información
+#           4 - Ver Stock
+#           5 - Ver item con mayor stock
+#           6 - Verificar un Item
+#           7 - Salir"
+#     opcion = gets.chomp.to_i
+#     case opcion
+#         when 1 
+#             puts 'Ingrese un Item y valor, separado por coma'
+#             ingreso = gets.chomp.gsub(' ', '_')
+#             arry1 = []
+#             arry1 = ingreso.scan(/\w+/)
+#             arry1[0] = arry1[0].to_sym
+#             arry2 = []
+#             arry2.push arry1
+#             inventario = inventario.merge(arry2.to_h)
+#             print  inventario
+#             puts ''
+#         when 2 
+#             puts 'Ingrese Item a Eliminar'
+#             ingreso = gets.chomp.gsub(' ', '_').to_sym
+#             inventario.delete(ingreso)
+#             puts inventario
+#         when 3 
+#             puts 'Ingresa el Item a Actualizar y su Stock'
+#             ingreso = gets.chomp.gsub(' ', '_')
+#             arry1 = []
+#             arry1 = ingreso.scan(/\w+/)
+#             arry1[0] = arry1[0].to_sym
+#             arry2 = []
+#             arry2.push arry1   
+#         when 4 
+#             puts '4'
+#         when 5 
+#             puts '5'
+#         when 6 
+#             puts '6'
+#     end 
+# end    
 
 
